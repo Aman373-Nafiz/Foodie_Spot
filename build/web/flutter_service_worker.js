cfb59801af615f6e95376e8d38d044a5b7447f07
Aman <amannafiz00@gmail.com>
2025-04-21 12:23:14 +0600

    Ready for Vercel web deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "33ddc384ed854a0e94bb1242ba3896c3
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5950552034e35074071dc9030a52e20c",
+"flutter_bootstrap.js": "7580aceab6f93595a8068ae0e8a40571",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "79b67d9f4800be17005ca5ae64a91ba6",
 "/": "79b67d9f4800be17005ca5ae64a91ba6",
-"main.dart.js": "6470ac39bb916a80f9f15a1e3f1215f7",
+"main.dart.js": "4c1bf9434fd6621b8caa317637fbd9ee",
 "manifest.json": "b177d1a338cfea6998412bd6808bb4a4",
 "version.json": "8a9829618ae4b48ae2ea66b8a58bd4ca"};
 // The application shell files that are downloaded before a service worker can
